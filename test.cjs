1bfd4ac919c814c1fc6dc3d4015f8ef447d21a4d
lucas354803 <lucaas3548@gmail.com>
2026-04-26 21:16:33 -0300

    Delete test.cjs

--- test.cjs
+++
@@ -1,9 +0,0 @@
-const fs=require('fs'), vm=require('vm');
-const code=fs.readFileSync('/mnt/data/rota_debug/inline.js','utf8');
-const ctx={console, window:{supabase:{createClient:()=>({})}}, localStorage:{getItem:(k)=>null,setItem:()=>{},removeItem:()=>{}}, setInterval:()=>{}, clearInterval:()=>{}, alert:console.log, document:{getElementById:()=>({classList:{add(){},remove(){}},innerHTML:''}), activeElement:null}};
-vm.createContext(ctx); vm.runInContext(code, ctx);
-vm.runInContext(`sessao={id:1,nome:'Lucas',tipo:'motoboy',telefone:'48999117385',whats:'48999117385',pix:'489'};
-motoboys=[{id:1,nome:'Lucas',tipo:'motoboy',telefone:'48999117385',whats:'48999117385',pix:'489'}];
-pedidos=[{id:9,cliente_nome:'big',loja:'Big Burger',status:'Entrega finalizada',motoboy_id:1,motoboy_nome:'Lucas',km:1.2,pagamento:'Pix',pagamento_status:'Pix confirmado',valor:8}];
-historicoEntregas=[]; historicoPagamentos=[];`, ctx);
-console.log(vm.runInContext('motoboyHTML()', ctx));
