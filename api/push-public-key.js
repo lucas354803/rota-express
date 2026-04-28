8acb3d0df5d4b22fbc09af533f42bf836375dc83
lucas354803 <lucaas3548@gmail.com>
2026-04-27 23:36:36 -0300

    Delete api directory

--- api/push-public-key.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== "GET") return res.status(405).json({ error: "Método não permitido" });
-  return res.status(200).json({ publicKey: process.env.VAPID_PUBLIC_KEY || "" });
-}
